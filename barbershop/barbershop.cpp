9f4de83159babc4df19960d0404b011b0c0c10df
Jack Margeson <margeson.jack@gmail.com>
2023-03-26 19:55:11 -0400

    Update barbershop

--- barbershop/barbershop.cpp
+++ barbershop/barbershop.cpp
@@ -23,24 +23,26 @@ long int customer_rate;
 // Define barber status enum
 enum enum_barber_status { AWAKE = true, ASLEEP = false};
 enum_barber_status barber_status = ASLEEP;
+// Define worker process management variable 
+bool worker_active = false;
 
 // Mutex lock semaphore, and queue variables.
 pthread_mutex_t mutex;
-sem_t semaphore;
 queue<int> customer_queue;
 
 // Code for the barber, the "worker thread"
 void* barber(void* arg) {
-    // Acquire the mutex lock
-    pthread_mutex_lock(&mutex);
-
-    while (true) {
+    while (worker_active) {
+        // Lock the mutex (nodifying the customer queue).
+        pthread_mutex_lock(&mutex);
         // Check the customer queue.
         if (!customer_queue.empty()) { // There are customers in the queue.
             // Pop the first customer from the queue.
             int customer = customer_queue.front();
             customer_queue.pop();
 
+            // Unlock the mutex (done with modifications to the customer queue)
+            pthread_mutex_unlock(&mutex);
             // Announce that a new customer is being processed.
             cout << "Customer #" << customer << " sits down in the barber's chair.\n";
 
@@ -62,19 +64,17 @@ void* barber(void* arg) {
                 cout << "There are no customers waiting. The barber has fallen asleep.\n";
                 barber_status = ASLEEP;
             }
+
+            // Unlock the mutex (done with modifications to the customer queue)
+            pthread_mutex_unlock(&mutex);
         }
-        // Release the mutex lock
-        pthread_mutex_unlock(&mutex);
-        // Signal that the barber is available
-        sem_post(&semaphore);
     }
     return NULL;
 }
 
 int main() {
-    // Initialize the mutex lock and semaphore variables
+    // Initialize the mutex lock
     pthread_mutex_init(&mutex, NULL);
-    sem_init(&semaphore, 0, 1);
 
     // Ask user how many customers they'd like to simulate 
     cout << "Barbershop Simulation\n" << \
@@ -95,19 +95,25 @@ int main() {
 
     // Initalize barber worker thread.
     barber_status = ASLEEP;
+    worker_active = true;
     pthread_t barber_thread;
     pthread_create(&barber_thread, NULL, barber, 0);
 
     // Enqueue customers.
     for (int i = 1; i < num_customers + 1; i++) {
+        // Lock the mutex (nodifying the customer queue).
+        pthread_mutex_lock(&mutex);
+
         if (customer_queue.size() >= max_chairs) {
             // Queue full, do not add.
             cout << "Customer #" << i << " arrives and sees that there is no room for them in the waiting room, so they leave.\n";
         } else {
             customer_queue.push(i);
             cout << "Customer #" << i << " arrives and sits in the waiting room. Current # of waiting customers: " << customer_queue.size() << "\n";
-
         }
+
+        // Unlock the mutex (done with modifications to the customer queue)
+        pthread_mutex_unlock(&mutex);
 
         // Wait customer_rate amount seconds before another customer arrives.
         sleep(customer_rate);
@@ -115,21 +121,24 @@ int main() {
 
     // Hold the main thread until the customer queue is empty.
     while (!customer_queue.empty()) {}
-    // Wait for the final customer to be processed.
-    // TODO: fix this
+    // Wait an additional time for the barber to finish with the last customer.
     sleep(barber_wait_time);
+    // Kill the barber.
+    worker_active = false;
 
     // Stop the chrono clock, print elapsed time in microseconds
     auto end_time = chrono::high_resolution_clock::now();
     auto elapsed_time = chrono::duration_cast<chrono::seconds>(end_time - start_time);
+    // Wait a second for the worker thread to finish being killed before printing results.
+    sleep(1);
+    // Print simulation results
     cout << "\nEnd simulation...\n" << \
             "-------------------\n";
     cout << "The barber shop is now closed.\n";
     cout << "Elapsed simulation time: " << elapsed_time.count() << " seconds" << endl;
 
-    // Destroy the mutex lock and semaphore variables
+    // Free the mutex lock.
     pthread_mutex_destroy(&mutex);
-    sem_destroy(&semaphore);
 
     return 0;
 }
